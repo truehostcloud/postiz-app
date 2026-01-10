9e169823f5053a131531bc3f8955791e917ea1d8
kipsang <kipsang1998@gmail.com>
2026-01-10 08:45:46 +0300

    fix: remove unnecessary comments

--- apps/frontend/src/app/(app)/auth/layout.tsx
+++ apps/frontend/src/app/(app)/auth/layout.tsx
@@ -19,7 +19,6 @@ export default async function AuthLayout({
 
   return (
     <div className="bg-[#0E0E0E] flex justify-center items-center flex-1 p-[12px] gap-[12px] min-h-screen w-screen text-white">
-      {/*<style>{`html, body {overflow-x: hidden;}`}</style>*/}
       <ReturnUrlComponent />
           <div className="flex justify-center items-center flex-col gap-4 text-start">
             <h1 className="text-[32px] font-[600]">Continue in Olitt</h1>
@@ -29,7 +28,7 @@ export default async function AuthLayout({
             <div>
               <a
                 href={olittAppUrl}
-                className="inline-flex items-center justify-center px-4 py-3 rounded-[10px] bg-white text-black font-[600] hover:opacity-90 transition-opacity"
+                className="inline-flex items-center  justify-center px-4 py-3 rounded-[10px] bg-white text-black font-[600] hover:opacity-90 transition-opacity"
               >
                 Back to Olitt
               </a>
